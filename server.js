e30fe9e9825bea46e681fe3a74694f1b224e1ce5
KhushiAgrawal263 <79684725+KhushiAgrawal263@users.noreply.github.com>
2023-06-29 21:20:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.static('uploads'))
 app.use(express.static('uploadFile'))
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.get("/",(req,res)=>{
     res.setHeader("Access-Control-Allow-Credentials","true");
     res.send("Api is running...")
